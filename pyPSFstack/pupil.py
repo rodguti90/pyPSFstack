f35ef63e18cabb210e6b2c57d39c420682fabc6f
Rodrigo <rodguti90@gmail.com>
2022-09-21 17:13:16 +0200

    Added plot fun, mesh only in pupil

--- pyPSFstack/pupil.py
+++ pyPSFstack/pupil.py
@@ -1,12 +1,6 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Dec 16 08:11:32 2021
-
-@author: rodrigo
-"""
-
+import matplotlib.pyplot as plt
 import numpy as np
+from .functions import colorize
 
 class Pupil():
     
@@ -15,15 +9,14 @@ class Pupil():
         self.aperture_size = aperture_size 
         self.computation_size = computation_size
         self.N_pts = N_pts
-        self.set_aperture()
+        self.step_f = self.computation_size/self.N_pts
+        # self.set_aperture()
 
-    def step_fourier(self):
-        return self.computation_size/self.N_pts
+    # def step_fourier(self):
+    #     return self.computation_size/self.N_pts
     
     def xy_mesh(self):
-        u_vec = np.arange(-self.computation_size/2,
-                        self.computation_size/2,
-                        self.step_fourier())
+        u_vec = np.arange(-1, 1 + self.step_f, self.step_f)
         return np.meshgrid(u_vec,u_vec)
     
     def polar_mesh(self):
@@ -32,17 +25,25 @@ class Pupil():
         uphi = np.arctan2(uy, ux)
         return ur, uphi
 
-    def set_aperture(self):
+    def get_aperture(self):
         ur, _ = self.polar_mesh()
-        self.aperture = np.empty((self.N_pts,self.N_pts,1,1), dtype=np.cfloat)
-        self.aperture[...,0,0] = ur**2 <= self.aperture_size**2
-           
-    # def update_pupil_parameters(self, pupil_params):
-    #     self.pupil_params = pupil_params
-    #     self.generate_pupil_array()
+        n_pts = ur.shape[0]
+        aperture = np.empty((n_pts,n_pts,1,1), dtype=np.cfloat)
+        aperture[...,0,0] = ur**2 <= self.aperture_size**2
+        return aperture
 
-    def generate_pupil_array(self):       
+    def get_pupil_array(self):       
         raise NotImplementedError("Please Implement this method")
+
+    def plot_pupil_field(self):
+        pupil = self.get_pupil_array()
+        sh = pupil.shape
+        if len(sh) == 4:
+            fig, axs = plt.subplots(sh[-2], sh[-1], figsize=(4*sh[-1],4*sh[-2]))
+            for r in range(sh[-2]):
+                for c in range(sh[-1]):
+                    axs[r,c].imshow(colorize(pupil[...,r,c]))
+                    axs[r,c].set_axis_off()
 
 
 
