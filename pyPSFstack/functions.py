03aa01712a9a19f88a44eb7d065fd64d1a142780
Rodrigo <rodguti90@gmail.com>
2022-09-21 17:09:43 +0200

    added colorize function

--- pyPSFstack/functions.py
+++ pyPSFstack/functions.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+from colorsys import hls_to_rgb
+import numpy as np
 # from scipy.optimize import minimize
 # from skimage.morphology import erosion, dilation
 
@@ -14,7 +15,31 @@ def trim_stack(stack, N_new):
                           (N_pts-N_new)//2:(N_pts+N_new)//2]
     return trimmed_stack
 
+def colorize(z, 
+             theme='dark', 
+             saturation=1., 
+             beta=1.4, 
+             transparent=False, 
+             alpha=1., 
+             max_threshold=1):
+             
+    r = np.abs(z)
+    r /= max_threshold*np.max(np.abs(r))
+    arg = np.angle(z) 
 
+    h = (arg + np.pi)  / (2 * np.pi) + 0.5
+    l = 1./(1. + r**beta) if theme == 'white' else 1.- 1./(1. + r**beta)
+    s = saturation
+
+    c = np.vectorize(hls_to_rgb) (h,l,s) # --> tuple
+    c = np.array(c)  # -->  array of (3,n,m) shape, but need (n,m,3)
+    c = np.transpose(c, (1,2,0))  
+    if transparent:
+        a = 1.-np.sum(c**2, axis = -1)/3
+        alpha_channel = a[...,None]**alpha
+        return np.concatenate([c,alpha_channel], axis = -1)
+    else:
+        return c
 
 # def outer_pixels(stack):
 #     shape_stack = list(stack.shape)
