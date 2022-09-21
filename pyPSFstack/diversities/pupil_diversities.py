7b358611fc44234d18934e96281c633fdddca4e9
Rodrigo Gutierrez-Cuevas <gutierrez.cuevas.rodrigo@gmail.com>
2022-09-21 21:44:21 +0200

    added DDiversity

--- pyPSFstack/diversities/pupil_diversities.py
+++ pyPSFstack/diversities/pupil_diversities.py
@@ -16,4 +16,19 @@ class ZDiversity(Pupil):
         return np.exp(1j*2*np.pi*self.nf*self.z_list*(1-ur**2)**(1/2))
 
 
-# class DDiversity(Pupil):
+class DDiversity(Pupil):
+    def __init__(self, diff_del_list, ni=1.33, nf=1.518, aperture_size = 1., computation_size=4., 
+                 N_pts=128):
+        Pupil.__init__(self, aperture_size, computation_size, N_pts)
+        
+        self.diff_del_list = np.reshape(np.array(diff_del_list), (1,1,-1))
+        self.N_ddiv = len(diff_del_list)
+        self.ni = ni
+        self.nf = nf  
+
+    def get_pupil_array(self):
+        ur, _ = self.polar_mesh()
+        ur = (ur[...,None]).astype(np.cfloat)
+        return np.exp(1j*2*np.pi*self.diff_del_list*self.ni
+                      *(1-(self.nf*ur/self.ni)**2)**(1/2)) 
+
