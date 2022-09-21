5772ba972705e83a20ba81ef397e4ab07ae40bd9
Rodrigo Gutierrez-Cuevas <gutierrez.cuevas.rodrigo@gmail.com>
2022-09-21 09:59:34 +0200

    Code simplification

--- pyPSFstack/core.py
+++ pyPSFstack/core.py
@@ -9,20 +9,24 @@ from functions import trim_stack
 from pyPSFstack_old.pupils.windows import NoPupil
 
 class PSFStack():
+
     def __init__(self, pupils=[NoPupil()], zdiversity=None, pdiversity=None):
         self.pupils = pupils
         self.N_pupils = len(self.pupils)
         self.N_pts = self.pupils[0].N_pts
         self.zdiversity = zdiversity
-        self.N_zdiv = zdiversity.N_zdiv
         self.pdiversity = pdiversity
-        self.N_pdiv = pdiversity.N_pdiv
+        # self.N_pdiv = pdiversity.N_pdiv
+        # self.N_zdiv = zdiversity.N_zdiv
 
     def compute_psf_stack(self):
         self._compute_compound_pupils()
-        self._compute_zdiv_pupil()
+        if self.zdiversity is not None:
+            self._compute_zdiv()
         self._propagate_image_plane()
-        self._compute_pdiv_psf_fields()
+        if self.pdiversity is not None:
+            self._compute_pdiv()
+
         self._incoherent_sum()
 
     def _compute_compound_pupils(self):
@@ -31,7 +35,7 @@ class PSFStack():
             self.compound_pupils += [self.pupils[ind+1].pupil_array 
                 @ self.compound_pupils[ind]]
         
-    def _compute_zdiv_pupil(self):
+    def _compute_zdiv(self):
         self.zdiv_pupil = self.compound_pupils[-1][...,np.newaxis,:,:] \
             * self.zdiversity.pupil_array[...,np.newaxis,np.newaxis]
 
@@ -42,27 +46,24 @@ class PSFStack():
                 axes=(0,1)), 
             axes=(0,1))/self.N_pts
 
-    def _compute_pdiv_psf_fields(self):
+    def _compute_pdiv(self):
         self.field_psf_stack = self.pdiversity.jones_list @ self.zdiv_psf[...,np.newaxis,:,:]
 
     def _incoherent_sum(self):
         self.psf_stack = np.sum(np.abs(self.field_psf_stack)**2,axis=(-2,-1))
 
 
-    def model_experimental_stack(self, bckgd_photons=20, N_photons=200, N_pts=None):
+    def model_experimental_stack(self, bckgd_photons=20, N_photons=200, bleach_amplitudes=1, N_pts=None):
         rng = np.random.default_rng()
         max_value = np.max(self.psf_stack)
         if N_pts is not None:
             stack = trim_stack(self.psf_stack, N_pts)/max_value
         else:
             stack = self.psf_stack/max_value
-        stack = np.round(N_photons*stack + bckgd_photons)
+        stack = np.round(N_photons*bleach_amplitudes*stack + bckgd_photons)
         stack = rng.poisson(stack)
         return stack
 
-    def _photobleach_background(self):
-        self.estimate_psf_stack = self.photobleach_amplitudes * self.psf_stack \
-            + self.background
     
 
     
