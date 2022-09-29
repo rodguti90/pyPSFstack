1af9e70daed32d6c28b1de076f2ea5d6b0da30d3
Rodrigo <rodguti90@gmail.com>
2022-09-29 16:07:22 +0200

    Added custom loss functions

---
+++ pyPSFstack_torch/cost_functions.py
@@ -0,0 +1,10 @@
+import torch
+
+def loss_loglikelihood(output, target):
+    loss = torch.mean(-target * torch.log(output) + output)
+    # loss = torch.mean(-target * torch.log(output/target) + output-target)
+    return loss
+
+def loss_sumsquared(output, target):
+    loss = torch.mean( (output-target)**2 )
+    return loss
