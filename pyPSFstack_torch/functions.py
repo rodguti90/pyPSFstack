990f95dbe0dcfa2145a08dd232dd1c433c4aa4de
Rodrigo Gutierrez-Cuevas <gutierrez.cuevas.rodrigo@gmail.com>
2022-10-04 21:22:23 +0200

    Added colorize

--- pyPSFstack_torch/functions.py
+++ pyPSFstack_torch/functions.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+from colorsys import hls_to_rgb
 from skimage.morphology import erosion, dilation
 
 # def cart2pol(x,y):
@@ -80,3 +81,29 @@ def estimate_background(stack,mask=None):
     """  
     background = np.mean(stack, axis=(0,1), where=mask)
     return background
+
+def colorize(z, 
+             theme='dark', 
+             saturation=1., 
+             beta=1.4, 
+             transparent=False, 
+             alpha=1., 
+             max_threshold=1):
+             
+    r = np.abs(z)
+    r /= max_threshold*np.max(np.abs(r))
+    arg = np.angle(z) 
+
+    h = (arg + np.pi)  / (2 * np.pi) + 0.5
+    l = 1./(1. + r**beta) if theme == 'white' else 1.- 1./(1. + r**beta)
+    s = saturation
+
+    c = np.vectorize(hls_to_rgb) (h,l,s) # --> tuple
+    c = np.array(c)  # -->  array of (3,n,m) shape, but need (n,m,3)
+    c = np.transpose(c, (1,2,0))  
+    if transparent:
+        a = 1.-np.sum(c**2, axis = -1)/3
+        alpha_channel = a[...,None]**alpha
+        return np.concatenate([c,alpha_channel], axis = -1)
+    else:
+        return c
