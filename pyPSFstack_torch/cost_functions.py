b5c173a6f811250dc0c7dea582d034adcaf04235
Rodrigo <rodguti90@gmail.com>
2022-10-05 17:30:56 +0200

    Log likelyhood norm by data

--- pyPSFstack_torch/cost_functions.py
+++ pyPSFstack_torch/cost_functions.py
@@ -1,8 +1,8 @@
 import torch
 
 def loss_loglikelihood(output, target):
-    loss = torch.mean(-target * torch.log(output) + output)
-    # loss = torch.mean(-target * torch.log(output/target) + output-target)
+    # loss = torch.mean(-target * torch.log(output) + output)
+    loss = torch.mean(-target * torch.log(output/target) + output-target)
     return loss
 
 def loss_sumsquared(output, target):
