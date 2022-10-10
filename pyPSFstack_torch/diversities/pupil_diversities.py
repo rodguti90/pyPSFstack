99cb490928a736c546e2425c323cb34839c0bd1d
Rodrigo Gutierrez-Cuevas <gutierrez.cuevas.rodrigo@gmail.com>
2022-10-10 21:50:05 +0200

    Added derivative diversity

--- pyPSFstack_torch/diversities/pupil_diversities.py
+++ pyPSFstack_torch/diversities/pupil_diversities.py
@@ -1,6 +1,11 @@
 import torch as torch
+import torch.nn as nn
 from ..pupil import torchPupilDiversity
 
+
+class torchNoDiversity(nn.Module):
+    def forward(self, input):
+        return input
 
 class torchZDiversity(torchPupilDiversity):
 
@@ -22,7 +27,34 @@ class torchZDiversity(torchPupilDiversity):
         ur = (ur[...,None]).type(torch.cfloat)
         zdiv = torch.exp(1j*2*torch.pi*self.nf*self.z_list*(1-ur**2)**(1/2))
         return zdiv.type(torch.cfloat)
-    
+
+class torchDerivativeDiversity(torchPupilDiversity):
+
+    def __init__(self, 
+                 m=2, 
+                 ni=1.33, 
+                 nf=1.518, 
+                 aperture_size = 1., 
+                 computation_size=4., 
+                 N_pts=128):
+
+        super(torchDerivativeDiversity, self).__init__(aperture_size, computation_size, N_pts)
+        
+        self.m = m
+        self.ni = ni
+        self.nf = nf  
+
+    def get_pupil_array(self):
+        ur, _ = self.polar_mesh()
+        nr, nc = ur.shape
+        ur = ur.type(torch.cfloat)
+        pupil_array = torch.empty((nr,nc,self.m+1), dtype=torch.cfloat)
+        for l in range(self.m+1):
+            pupil_array[...,l] = (1j*2*torch.pi*self.ni*(1-(self.nf*ur/self.ni)**2)**(1/2))**l
+        
+        return pupil_array
+
+
 class torchDDiversity(torchPupilDiversity):
 
     def __init__(self, 
