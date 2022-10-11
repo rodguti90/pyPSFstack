96ee727a6fcf957cea02e78353db232996e3f454
Rodrigo <rodguti90@gmail.com>
2022-10-11 16:04:51 +0200

    Finished implementing blurring

--- pyPSFstack_torch/blurring/blurring.py
+++ pyPSFstack_torch/blurring/blurring.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 from math import factorial
 
 from ..diversities.pupil_diversities import torchNoDiversity, torchDerivativeDiversity
-from ..blurring.kernels import torchBKSASphere
+from ..blurring.kernels import torchBKSASphere, torchBK2DSphere
 
 class torchBlurring(nn.Module):
     def __init__(self):
@@ -22,11 +22,38 @@ class torchNoBlurring(torchBlurring):
     def forward(self, input):
         return self._compute_intensity(input)
 
+class torch2DBlurring(torchBlurring):
+
+    def __init__(self,
+                 radius=0.,
+                 emission='sphere',
+                 nf=1.518,          
+                 aperture_size=1., 
+                 computation_size=4., 
+                 N_pts=128,
+                 ):
+
+        super(torch2DBlurring, self).__init__()
+
+
+        if emission == 'sphere':
+            self.bk = torchBK2DSphere(
+                               radius,
+                               nf,
+                               aperture_size=2*aperture_size,
+                               computation_size=computation_size,
+                               N_pts=N_pts)
+
+    def forward(self, input):
+        output = self._compute_intensity(input)
+        output = self.bk(output)
+        return torch.abs(output)
+
+
 class torchSABlurring(torchBlurring):
 
     def __init__(self,
                  radius=0.,
-                 m=2,
                  emission='sphere',
                  ni=1.33,
                  nf=1.518,          
@@ -37,16 +64,15 @@ class torchSABlurring(torchBlurring):
 
         super(torchSABlurring, self).__init__()
 
-        self.diversity = torchDerivativeDiversity(m=m, 
+        self.diversity = torchDerivativeDiversity(
                                     ni=ni, 
                                     nf=nf, 
                                     aperture_size=aperture_size, 
                                     computation_size=computation_size, 
                                     N_pts=N_pts)
-        self.m_max = m
 
         if emission == 'sphere':
-            self.bk = torchBKSASphere(m,
+            self.bk = torchBKSASphere(
                                radius,
                                nf,
                                aperture_size=2*aperture_size,
@@ -54,23 +80,21 @@ class torchSABlurring(torchBlurring):
                                N_pts=N_pts)
 
     def forward(self, input):
-        output = self._compute_even_intensity_der(input)
+        output = self._compute_0N2_intensity_der(input)
         output = self.bk(output)
         return torch.abs(output)
 
-    def _compute_even_intensity_der(self, input, orientation):
-    
+    def _compute_0N2_intensity_der(self, input):
     
         field_m = input
-        
         in_sh = list(input.shape)
-        l_max = self.m_max//2 +1
-        int_m = torch.zeros(in_sh[:2]+[l_max]+in_sh[3:], dtype=torch.cfloat)
+        l_max = 2
+        int_m = torch.zeros(in_sh[:2]+[2]+in_sh[3:], dtype=torch.cfloat)
 
-        for l in range(l_max):
-            for k in range(2*l+1):
-                int_m[:,:,l,...] += (factorial(2*l)/(factorial(k)*factorial(2*l-k))) * \
-                    torch.conj(field_m[:,:,k,...]) * field_m[:,:,2*l-k,...]
+        int_m[:,:,0,...] = torch.abs(field_m[:,:,0,...])**2
+        int_m[:,:,1,...] = torch.conj(field_m[:,:,0,...]) * field_m[:,:,2,...] \
+                            + 2 *  torch.abs(field_m[:,:,1,...])**2  \
+                            + field_m[:,:,0,...] * torch.conj(field_m[:,:,2,...] )
         
         return torch.sum(int_m, dim=(-2,-1))
         
--- pyPSFstack_torch/blurring/kernels.py
+++ pyPSFstack_torch/blurring/kernels.py
@@ -1,13 +1,32 @@
 import torch
-from math import factorial
-
 
 from ..pupil import torchBlurringKernel
 
 
+class torchBK2DSphere(torchBlurringKernel):
+    def __init__(self, 
+                 radius,
+                 nf,
+                 aperture_size=2.,
+                 computation_size=4,
+                 N_pts=128):
+
+        super(torchBK2DSphere, self).__init__(aperture_size, computation_size, N_pts)
+        
+        self.nf = nf
+        self.radius = radius
+
+    def get_pupil_array(self):
+        ur, _ = self.polar_mesh(umax=self.computation_size/2)
+        # ur = ur.type(torch.cfloat)
+        
+        bk = self.nf**2 * self.radius**3 * ja1(2*torch.pi*self.nf*self.radius*ur)
+        ap = self.get_aperture(umax=self.computation_size/2, dummy_ind=0)
+
+        return bk * ap
+
 class torchBKSASphere(torchBlurringKernel):
     def __init__(self, 
-                 m,
                  radius,
                  nf,
                  aperture_size=2.,
@@ -15,28 +34,39 @@ class torchBKSASphere(torchBlurringKernel):
                  N_pts=128):
 
         super(torchBKSASphere, self).__init__(aperture_size, computation_size, N_pts)
-        self.l_max = m//2
+        
         self.nf = nf
         self.radius = radius
 
     def get_pupil_array(self):
         ur, _ = self.polar_mesh(umax=self.computation_size/2)
         nr, nc = ur.shape
-        ur = ur.type(torch.cfloat)
-        bk = torch.empty((nr,nc,self.l_max+1), dtype=torch.cfloat) 
+        # ur = ur.type(torch.cfloat)
+        bk = torch.empty((nr,nc,2), dtype=torch.cfloat) 
         
-        origin = ur == 0
-        for l in range(self.l_max+1):
-            pref = (self.nf*self.radius)**(l+2) / (2**l * factorial(l) *self.nf**(2*l+1))
-            bessel_term = spherical_jn(l+1, 2*torch.pi*self.nf*self.radius*ur)/(2*torch.pi*ur)**(l+1)
-            bessel_term[origin] = 2**l * factorial(l) * self.nf * self.radius / factorial(2*l)
-            bk[...,l] = pref * bessel_term
+        bk[...,0] = self.nf**2 * self.radius**3 * ja1(2*torch.pi*self.nf*self.radius*ur)
+        bk[...,1] = self.nf**2 * self.radius**5 *ja2(2*torch.pi*self.nf*self.radius*ur) / 2 
 
         ap = self.get_aperture(umax=self.computation_size/2, dummy_ind=1)
         return bk * ap
 
-def spherical_j1():
-    pass
+def ja1(x):
+    l_val = x > 0.1
+    s_val = torch.logical_not(l_val)
+    output = torch.zeros_like(x)
+    xl = x[l_val]
+    xs = x[s_val]
+    output[l_val] = torch.sin(xl)/xl**3 - torch.cos(xl)/xl**2
+    output[s_val] = 1 / 3 -  xs**2 / 30
+    return output 
 
-def spherical_j2():
-    pass
+
+def ja2(x):
+    l_val = x > 0.1
+    s_val = torch.logical_not(l_val)
+    output = torch.zeros_like(x)
+    xl = x[l_val]
+    xs = x[s_val]
+    output[l_val] = (torch.sin(xl)*(3/xl**2-1) - 3*torch.cos(xl)/xl)/xl**3
+    output[s_val] =  1/15 - xs**2 / 210
+    return output 
